6c642fded99c8d8378b2800cfa868101e744b8f8
Francoedson <franco.edson.18014@gmail.com>
2023-10-15 20:24:36 -0500

    Creando el proyecto API WEB .NET Framework

---
+++ solCruzSacoSoft/CruzSacoSoft/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace CruzSacoSoft.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ solCruzSacoSoft/CruzSacoSoft/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CruzSacoSoft.WebApiApplication" Language="C#" %>
