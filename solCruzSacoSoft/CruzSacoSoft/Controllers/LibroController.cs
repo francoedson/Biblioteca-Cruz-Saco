ef8eb27de25b9d9190731594297a2095633c53ef
Francoedson <franco.edson.18014@gmail.com>
2023-10-17 23:43:52 -0500

    Controlador API Libro

---
+++ solCruzSacoSoft/CruzSacoSoft/Controllers/LibroController.cs
@@ -0,0 +1,60 @@
+﻿using CruzSacoSoft.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace CruzSacoSoft.Controllers
+{
+    public class LibroController : ApiController
+    {
+
+        // Define la cadena de conexión usando la configuración.
+        string con = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+
+        // GET: api/Editorial
+        [HttpGet]
+        [Route("api/Libro/listar")]
+        public IEnumerable<Libro> Get()
+        {
+            List<Libro> libro = new List<Libro>();
+
+            using (SqlConnection connection = new SqlConnection(con))
+            {
+                connection.Open();
+                using (SqlCommand cmd = new SqlCommand("sp_ListarLibro", connection))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Libro l = new Libro
+                            {
+                                codLibro = reader["codigo"].ToString(),
+                                titulo = reader["titulo"].ToString(),
+                                autor = reader["autor"].ToString(),
+                                categoria = reader["categoria"].ToString(),
+                                editorial = reader["editorial"].ToString(),
+                                ubicacion = reader["Ubicacion"].ToString(),
+                                ejemplares = (int)reader["cantidad"],
+                                vchEstado = reader["estado"].ToString(),
+                                fechaCreacion = reader["Fecha"].ToString(),
+                            };
+                            libro.Add(l);
+                        }
+                    }
+                }
+            }
+            return libro;
+        }
+
+
+    }
+}
---
+++ solCruzSacoSoft/CruzSacoSoft/Models/Libro.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CruzSacoSoft.Models
+{
+    public class Libro
+    {
+        public string codLibro { get; set; }
+        public string titulo { get; set; }
+        public string autor { get; set; }
+        public string categoria { get; set; }
+        public string editorial { get; set; }
+        public string ubicacion { get; set; }
+        public int ejemplares{ get; set; }
+        public string vchEstado { get; set; }
+        public int intEstado { get; set; }
+        public string fechaCreacion { get; set; }
+
+    }
+}
